01a69db4639c47196f0fa711a5a6370c2431a4ec
Arya2422 <123355820+Arya2422@users.noreply.github.com>
2023-08-15 18:26:25 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,7 @@
+function toggle_visibility(){
+    var e=documnet.getElementById('feedback-main');
+    if(e.style.display=='block')
+    e.style.display='none';
+    else
+    e.style.display='block';
+}
